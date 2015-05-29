d6d045268ffd7cea2946db1a4de999ff2d1f3a52
Matthew R Hanlon <mrhanlon@tacc.utexas.edu>
2015-05-29 16:48:29 -0500

    removed files

--- spec/browser/driver.js
+++
@@ -1,9 +0,0 @@
-/*
- * Web driver manager
-*/
-'use strict';
-
-var webdriver = require('selenium-webdriver');
-var driver = new webdriver.Builder().forBrowser('chrome').build();
-
-module.exports = driver;
